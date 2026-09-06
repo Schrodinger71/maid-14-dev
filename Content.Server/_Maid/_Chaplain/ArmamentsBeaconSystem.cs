a3d1709ad9287a5d33d444f00f936c7029cebf2e
Schrödinger <132720404+Schrodinger71@users.noreply.github.com>
2026-09-06 22:52:35 +0300

    feat: добавление системы ArmamentsBeacon с интерфейсом выбора брони и логикой обработки событий

---
+++ Content.Server/_Maid/_Chaplain/ArmamentsBeaconSystem.cs
@@ -0,0 +1,35 @@
+using Content.Shared._Maid.Chaplain;
+using Content.Shared.Inventory;
+
+namespace Content.Server._Maid.Chaplain;
+
+public sealed class ArmamentsBeaconSystem : EntitySystem
+{
+    [Dependency] private readonly InventorySystem _inventorySystem = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<ArmamentsBeaconComponent, ArmorSelectedEvent>(OnArmorSelected);
+    }
+
+    private void OnArmorSelected(Entity<ArmamentsBeaconComponent> ent, ref ArmorSelectedEvent args)
+    {
+        var entity = args.Actor;
+        var index = args.SelectedIndex;
+
+        if (index < 0 || index >= ent.Comp.Armor.Count)
+            return;
+        _inventorySystem.TryUnequip(entity, "outerClothing", true);
+        _inventorySystem.SpawnItemInSlot(entity, "outerClothing", ent.Comp.Armor[index], silent: true);
+
+        if (index < ent.Comp.Helmets.Count && ent.Comp.Helmets[index] != null)
+        {
+            _inventorySystem.TryUnequip(entity, "head", true);
+            _inventorySystem.SpawnItemInSlot(entity, "head", ent.Comp.Helmets[index]!.Value, silent: true);
+        }
+
+        Del(ent);
+    }
+}
